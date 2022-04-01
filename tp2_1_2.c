d24c04b551ba22363aa70a64e9d1d8347e7033d1
Agustin978 <loboagus10@gmail.com>
2022-04-01 08:19:21 -0300

    Correcciones del archivo en rama opcion_2

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -10,24 +10,13 @@ int main()
     int i;
     double vt[N];
     srand((int)time(NULL));
-    ptr = &vt[0];
-    //printf("\n%d\n\n", ptr);
-    //printf("%d\n\n", *(vt));
-    //printf("\n%d", *(vt+i));
     printf("\n");
     
     for(i = 0;i<N; i++)
     {
-        printf("%f / ", &ptr);
-        ptr++;
-    }
-
-    printf("\n\n\n");
-    for(i = 0;i<N; i++)
-    {
         vt[i]=1+rand()%100;
-        printf("%f / ", vt[i]);
+        ptr = &vt[i];
+        printf("%f / ", *ptr);
     }
-
     return 0;
 }
